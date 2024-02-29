7ffb7e2b051aea1ec09fe4446eccaf04ac65d463
mwdziak <mwdziak@gmail.com>
2024-02-29 10:30:14 +0100

    Basic implementation of user entity and database connection properties setup, post and feed fields in UserEntity yet to be implemented.

---
+++ src/main/java/com/example/instagrambackend/domain/UserEntity.java
@@ -0,0 +1,58 @@
+package com.example.instagrambackend.domain;
+
+import org.springframework.data.neo4j.core.schema.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Node("User")
+public class UserEntity implements UserDetails {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String username;
+    private String password;
+    private String email;
+    private String firstName;
+    private String lastName;
+    @Relationship(type = "FOLLOWS", direction = Relationship.Direction.OUTGOING)
+    private List<UserEntity> followedBy;
+    @Relationship(type = "FOLLOWS", direction = Relationship.Direction.INCOMING)
+    private List<UserEntity> followers;
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
